483f69ba4da5cd44a5eeca55477d306cca3c591c
Teerpong Phothiphun <teerpong.me@gmail.com>
2017-11-01 16:35:59 +0700

    Add more quote to binary path in snappy config

--- config/snappy.php
+++ config/snappy.php
@@ -5,14 +5,14 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => env('WKHTMLTOPDF_BINARY_PATH', 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf'),
+        'binary'  => '"' . env('WKHTMLTOPDF_BINARY_PATH', 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf') . '"',
         'timeout' => env('WKHTMLTOPDF_TIMEOUT', 500),
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => env('WKHTMLTOPDF_BINARY_PATH', 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf'),
+        'binary'  => '"' . env('WKHTMLTOPDF_BINARY_PATH', 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf') . '"',
         'timeout' => env('WKHTMLTOPDF_TIMEOUT', 500),
         'options' => array(),
         'env'     => array(),
